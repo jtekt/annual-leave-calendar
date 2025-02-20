788424fdb9c65be16464a5c754d57cd79f1fd7cd
Iida <00189119@jtekt.co.jp>
2025-02-20 11:40:01 +0900

    #7 modelsのallocation.tsのコレクション名をallocationからallocationsに修正しました

--- models/allocation.ts
+++ models/allocation.ts
@@ -2,23 +2,22 @@ import { Schema, model } from "mongoose"
 import IAllocation from "../interfaces/allocation"
 
 const schema = new Schema<IAllocation>({
-    year: { type: Number, required: true },
-    user_id: { type: String, required: true },
-    leaves: {
-      current_year_grants: { type: Number, default: 0, required: true },
-      carried_over: { type: Number, default: 0, required: true },
-     },
-     reserve: {
-      current_year_grants: { type: Number, default: 0, required: true },
-      carried_over: { type: Number, default: 0, required: true },
-     },
-  })
-  
-  schema.index({ year: 1, user_id: 1 }, { unique: true })
-  schema.index({ user_id: 1 })
-  schema.index({ year: 1 })
-  
-  const Model = model("allocation", schema)
-  
-  export default Model
-  
+  year: { type: Number, required: true },
+  user_id: { type: String, required: true },
+  leaves: {
+    current_year_grants: { type: Number, default: 0, required: true },
+    carried_over: { type: Number, default: 0, required: true },
+  },
+  reserve: {
+    current_year_grants: { type: Number, default: 0, required: true },
+    carried_over: { type: Number, default: 0, required: true },
+  },
+})
+
+schema.index({ year: 1, user_id: 1 }, { unique: true })
+schema.index({ user_id: 1 })
+schema.index({ year: 1 })
+
+const Model = model("allocations", schema)
+
+export default Model
