cb1f011bc23396f76a4e35809329b1701315a9c9
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2025-07-08 12:51:32 +0900

    Authentication for oidc and legacy

--- routes/index.ts
+++ routes/index.ts
@@ -2,8 +2,11 @@ import { Router } from "express"
 import router_v1 from "./v1/index"
 import router_v2 from "./v2/index"
 import router_v3 from "./v3/index"
+import { getMiddlewareChain } from "../auth";
 
-const router = Router()
+const router = Router();
+
+router.use(getMiddlewareChain());
 
 router.use("/", router_v1)
 router.use("/v1", router_v1)
